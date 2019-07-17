86fd34e415e655a7e66ba4aaedabb02052c5bb62
zxk7516 <zxk7516@gmail.com>
2019-07-17 17:53:23 +0800

    asdfasf

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
 #![allow(unused_imports)]
 
- #[macro_use]
-extern crate diesel;
+#![allow(proc_macro_derive_resolution_fallback)]
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate serde_derive;
  
  use actix_web::{web as a_web, App, HttpRequest, HttpServer, Responder};
+ use actix::prelude::{Addr,SyncArbiter};
+ use dotenv::dotenv;
+ use std::env;
+use diesel::{r2d2::ConnectionManager, PgConnection};
 
  mod schema;
  mod web;
+ mod models;
+
+ use models::DbExecutor;
 
 fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
+pub struct AppState {
+    pub db: Addr<DbExecutor>,
+}
+
 fn main() {
-    HttpServer::new(|| {
+
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    // let address :Addr<DbExecutor>  = SyncArbiter::start(4, move || DbExecutor(pool.clone()));
+
+    
+
+    
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/", a_web::get().to(greet))
             .route("/hi/{name}", a_web::get().to(greet))
             .route("/auth/login", a_web::post().to(web::auth::login))
---
+++ src/models.rs
@@ -0,0 +1,27 @@
+
+use diesel::{self, prelude::*};
+use chrono::NaiveDateTime;
+use uuid::Uuid;
+use crate::schema::{users};
+
+use actix::{Actor, SyncContext};
+use diesel::pg::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
+
+pub struct DbExecutor(pub Pool<ConnectionManager<PgConnection>>);
+
+
+impl Actor for DbExecutor {
+    type Context = SyncContext<Self>;
+}
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "users"]
+pub struct User {
+    
+    pub email: String,
+    pub password: String,
+    pub created_at: NaiveDateTime, // only NaiveDateTime works here due to diesel limitations
+    pub updated_at: NaiveDateTime, // only NaiveDateTime works here due to diesel limitations
+}
+
